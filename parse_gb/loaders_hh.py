ebdf71c064338d26c9da1c678c3f490064c68326
eyakovleva <ekaterinajakovl8@gmail.com>
2021-04-20 01:24:44 +0300

    added author_parse

--- parse_gb/loaders_hh.py
+++ parse_gb/loaders_hh.py
@@ -6,13 +6,42 @@ from scrapy.loader import ItemLoader
 from itemloaders.processors import MapCompose, TakeFirst
 
 
+def get_author(author):
+    url = 'https://hh.ru/'
+    user_link = urljoin(url, author)
+    return user_link
+
+
+def flat_text(items):
+    return "".join(items)
+
+
+def text_with_del(items):
+    new_text = "\n".join(items)
+    new_text.replace('\n', '', 3)
+    return new_text
+
+
+def make_a_list(item):
+    return item[0].split(", ")
+
+
 class HHLoader(ItemLoader):
     default_item_class = dict
-    # url_out = TakeFirst()
-    # title_out = TakeFirst()
-    # description_out = TakeFirst()
-    # price_in = MapCompose(clear_price)
-    # price_out = TakeFirst()
-    # author_in = MapCompose(get_author_id)
-    # author_out = TakeFirst()
-    # characteristics_in = MapCompose(get_characteristic)
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    description_in = text_with_del
+    description_out = text_with_del
+    salary_in = flat_text
+    salary_out = flat_text
+    author_in = MapCompose(get_author)
+    author_out = TakeFirst()
+
+
+class AuthorLoader(ItemLoader):
+    default_item_class = dict
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    activity_in = make_a_list
+    description_out = TakeFirst()
+
--- parse_gb/spiders/headhunter.py
+++ parse_gb/spiders/headhunter.py
@@ -1,5 +1,5 @@
 import scrapy
-from parse_gb.loaders_hh import HHLoader
+from parse_gb.loaders_hh import HHLoader, AuthorLoader
 
 
 class HeadhunterSpider(scrapy.Spider):
@@ -9,14 +9,23 @@ class HeadhunterSpider(scrapy.Spider):
 
     _xpath_selectors = {
         "pagination": "//span[contains(@class, 'bloko-button')]//a[contains(@class, 'bloko-button')]/@href",
-        "vacancy": "//span[contains(@class, 'g-user-content')]//a[contains(@data-qa, 'vacancy-serp')]/@href"
+        "vacancy": "//span[contains(@class, 'g-user-content')]//a[contains(@data-qa, 'vacancy-serp')]/@href",
+        "author": "//div[contains(@class, 'vacancy-serp')]//a[contains(@href, 'employer')]/@href"
     }
     _xpath_data_query = {
         "title": "//div[@class='vacancy-title']//h1[contains(@data-qa, 'vacancy-title')]/text()",
         "salary": "//p[contains(@class, 'vacancy-salary')]//span[contains(@data-qa, 'bloko-header-2')]/text()",
         "description": '//div[@class="vacancy-description"]//text()',
-        "key qualities": "//div[@class='vacancy-section']//div[contains(@class, 'bloko-tag')]//text()",
+        "key_qualities": "//div[@class='vacancy-section']//div[contains(@class, 'bloko-tag')]//text()",
         "author": "//a[@class='vacancy-company-name']/@href"
+    }
+
+    _xpath_author_query = {
+        'title': '//span[contains(@data-qa, "title-name")]/text()',
+        # 'url': '//link[@rel="canonical"]/@href',
+        'activity': "//div[@class='employer-sidebar-block']//p/text()",
+        'description': "//div[@data-qa='company-description-text']//div/p/text()"
+
     }
 
     def _get_follow_xpath(self, response, selector, callback, **kwargs):
@@ -30,11 +39,21 @@ class HeadhunterSpider(scrapy.Spider):
         yield from self._get_follow_xpath(
             response, self._xpath_selectors["vacancy"], self.vacancy_parse,
         )
+        yield from self._get_follow_xpath(
+            response, self._xpath_selectors["author"], self.author_parse,
+        )
 
 
     def vacancy_parse(self, response):
         loader = HHLoader(response=response)
         loader.add_value("url", response.url)
         for key, selector in self._xpath_data_query.items():
+            loader.add_xpath(key, selector)
+        yield loader.load_item()
+
+    def author_parse(self, response):
+        loader = AuthorLoader(response=response)
+        loader.add_value("url", response.url)
+        for key, selector in self._xpath_author_query.items():
             loader.add_xpath(key, selector)
         yield loader.load_item()
